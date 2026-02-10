878902ebab0f8bd45ffb33b6a6f0bc31000dbade
irahulk01 <irahulkv@gmail.com>
2026-02-10 21:27:48 +0530

    cors error fixed

--- src/routes/contactform.js
+++ src/routes/contactform.js
@@ -1,9 +1,13 @@
-import express from 'express';
-import { getContacts, submitContact } from '../controllers/contactFormController.js';
+
+import express from "express";
+import { getContacts, submitContact } from "../controllers/contactFormController.js";
 
 const router = express.Router();
 
-router.get('/', getContacts);
-router.post('/', submitContact);
+// GET all contacts
+router.get("/getContacts", getContacts);
+
+// POST submit contact
+router.post("/submitContact", submitContact);
 
 export default router;
